6bc35164ab7858f830ae6a1e7e103617d28bf2dc
Ankush Thakur <ankush.thakur53@gmail.com>
2020-08-29 18:13:08 +0530

    add logger middleware

---
+++ app/middleware/logger.js
@@ -0,0 +1,9 @@
+const logger = (req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    console.log(`\n${req.method} ${req.url}\n`);
+    console.log(`Request Body = ${req.body}`);
+  }
+  next();
+};
+
+module.exports = logger;
