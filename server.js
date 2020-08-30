862de17e625452766f0a171d8d21139aec1e30cd
Ankush Thakur <ankush.thakur53@gmail.com>
2020-08-30 19:34:47 +0530

    Add working app with dummy routes

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const routes = require("./app/router");
 const logger = require("./app/middleware/logger");
 
 const app = express();
 app.options("*", cors());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json());
 app.use(logger);
+app.use("/api/v1", routes);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
