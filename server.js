5e454547982f0eb515d25b8a0cfcf8f738496c6e
Ankush Thakur <ankush.thakur53@gmail.com>
2020-09-02 00:46:12 +0530

    Fix CORS allowed-headers error

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const logger = require("./app/middleware/logger");
 const app = express();
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, content-type");
   next();
 });
 app.use(bodyParser.json());
